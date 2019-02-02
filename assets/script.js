f76878d82b32d31b04ce1aaf8af6abbf0a36c1af
Kwinten Olaerts <kwintenolaerts@hotmail.com>
2019-02-02 18:08:56 +0100

    basis setup

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+function rst() {
+    console.log('Click Reset')
+    // var clear = document.querySelector("#name").value = "";
+}
+
+function show() {
+    alert("Are you sure?")
+    alert('are you sure?')
+}
