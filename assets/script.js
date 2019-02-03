c21a962838e8423ab6bb9fd1181e50e58364b92c
Kwinten Olaerts <kwintenolaerts@hotmail.com>
2019-02-03 11:25:12 +0100

    SuperVillian app

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,22 @@
-function rst() {
-    console.log('Click Reset')
-    // var clear = document.querySelector("#name").value = "";
+let tonen = document.getElementsByTagName("h1")
+console.log(tonen)
+function show(){r
+result[0].innerHTML ="the villain: " + nameVillain.value + " with the " + superpower.value + " wants to take over the world because " + motivation.value;
+nameVillain.value="";
+superpower.value="";
+motivation.value="";
 }
 
-function show() {
-    alert("Are you sure?")
-    alert('are you sure?')
-}
+// function show() {
+//     nameVillain.value = "";
+//     superpower.value = "";
+//     motivation.value = "";
+//     plan.value = "";
+//   }
+
+  function rst() {
+    nameVillian.value = "";
+    superpower.value = "";
+    motivation.value = "";
+    plan.value = "";
+  }
